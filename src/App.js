e3a63270b941874f6010cc698c93a82b81f3141b
Michael Doney <michaeldoney@comcast.net>
2019-10-08 00:39:53 -0400

    User page: user details, UI enhancements, repos
    Signed-off-by: Michael Doney <michaeldoney@comcast.net>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import axios from 'axios';
 import Alert from './components/layout/Alert';
@@ -14,19 +15,55 @@ class App extends Component {
     users: [],
     loading: false,
     alert: null,
+    user: {},
+    repos: [],
   };
 
+  //Searches Github Users api for the given text
   searchUsers = (queryText) => {
-    debugger;
     if (queryText) {
+      this.setState({ loading: true });
       const url = `https://api.github.com/search/users?q=${queryText}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`;
 
       axios.get(url).then((response) => {
         this.setState(
           {
+            loading: false,
             users: response.data.items
           })
       });
+    }
+  }
+
+  //Gets a single Github user
+  getUser = async username => {
+    if (username) {
+      this.setState({ loading: true });
+      const url = `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`;
+
+      const response = await axios.get(url);
+
+      this.setState(
+        {
+          loading: false,
+          user: response.data
+        })
+    }
+  }
+
+  //Get user repos
+  getUserRepos = async username => {
+    if (username) {
+      this.setState({ loading: true });
+      const url = `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`;
+
+      const response = await axios.get(url);
+
+      this.setState(
+        {
+          loading: false,
+          repos: response.data
+        })
     }
   }
 
@@ -37,7 +74,7 @@ class App extends Component {
     });
   }
 
-  // Set Alert
+  // Set Alert -- goes away after 2 seconds
   setAlert = (msg, type) => {
     this.setState({
       alert: { msg, type }
@@ -51,7 +88,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading, alert } = this.state;
+    const { users, loading, alert, user, repos } = this.state;
 
     return (
       <Router>
@@ -60,7 +97,6 @@ class App extends Component {
           <div className="container">
             <Alert alert={alert} />
             <Switch>
-              <Route exact path='/about' component={About} />
               <Route exact path='/' render={props => (
                 <Fragment>
                   <Search
@@ -72,8 +108,21 @@ class App extends Component {
                   <Users
                     users={users}
                     loading={loading}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                   />
                 </Fragment>
+              )} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  user={user}
+                  loading={loading}
+                  getUserRepos={this.getUserRepos}
+                  repos={repos}
+                />
               )} />
             </Switch>
           </div>
